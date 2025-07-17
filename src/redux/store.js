bc3592b31a246de4e50cc15cfd65d5f857dce0a3
Evgenius Sych <evgenius007007@gmail.com>
2025-07-17 22:31:51 +0300

    edit of store.js, unnecessary code removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,4 @@
 import { appReducer } from './reducers'
 import { createStore } from 'redux'
 
-// export const createStore = (appReducer) => {
-// 	let state
-// 	const subscribers = []
-
-// 	return {
-// 		dispatch(action) {
-// 			state = appReducer(state, action)
-// 			subscribers.forEach((sub) => sub())
-// 		},
-// 		subscribe(callback) {
-// 			subscribers.push(callback)
-// 		},
-// 		getState() {
-// 			return state
-// 		},
-// 	}
-// }
-
 export const store = createStore(appReducer)
-store.dispatch({ type: 'ddd' })
